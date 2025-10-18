305069238511688897c89be9e0142a7af9ba1083
efeturkol <mahmutefeturkol@hotmail.com>
2025-10-19 01:23:28 +0300

    second commit

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ class Musteri:
         
 
 class Hesap:
-    def __init__(self,iban,musteri,bakiye=0.0,borc=0.0, **islem):
+    def __init__(self,iban,musteri,bakiye=0.0,borc=0.0):
         self.iban = iban
         self.musteri = musteri
         self.bakiye = bakiye
         self.borc = borc
-        self.islem = islem 
+        self.islem = []
 
     def para_yatir(self,miktar):
         if miktar > 0:
@@ -33,7 +33,7 @@ class Hesap:
             print("Yetersiz bakiye!")
         else:
             self.bakiye -= miktar
-            self.islem.append(islemKaydi("Para çekme",-miktar))
+            self.islem.append(IslemKaydi("Para çekme",-miktar))
             print(f"{miktar}₺ çekildi. Kalan bakiye: {self.bakiye}₺")
 
     def borc_yatirma(self,miktar):
@@ -42,11 +42,36 @@ class Hesap:
         elif miktar > self.borc:
             print("Borç miktarından fazla ödeme yapılamaz.")
         elif miktar > self.bakiye:
-              
+            print("Yetersiz bakiye!")
+        else:
+            self.bakiye -= miktar
+            self.borc -= miktar
+            self.islem.append(IslemKaydi("Borç Ödeme: ", -miktar))
+            print(f"{miktar}₺ borç ödendi. Kalan borç: {self.borc}₺")
+    
+    def transfer_yap(self,farkli_musteri,miktar):
+        if miktar <= 0:
+            print("Geçersiz miktar!")
+        elif miktar > self.bakiye:
+            print("Yetersiz bakiye!")
+        else:
+            self.bakiye -= miktar 
+            farkli_musteri.bakiye += miktar
+            self.islem.append(IslemKaydi(f"{farkli_musteri.musteri.ad} adlı kişiye transfer", -miktar))
+            farkli_musteri.islem.append(IslemKaydi(f"{self.ad} adlı kişiden transfer", miktar))
+            print(f"{farkli_musteri.ad} adlı kişiye {miktar}₺ gönderildi.")
+
+    def bilgi_goster(self):
+        print(f"\n--- {self.musteri.ad} {self.musteri.soyad} ---")
+        print(f"TC: {self.musteri._Musteri__tc}")
+        print(f"Bakiye: {self.bakiye}₺")
+        print(f"Borç: {self.borc}₺")
+        print("İşlem Geçmişi:")
+        for islem in self.islem:
+            print(" ", islem)
+        print("-----------------------------\n")
 
             
-
-
 
 class Banka:
     def __init__(self,ad,**musteriler):
@@ -54,7 +79,7 @@ class Banka:
         self.musteriler = musteriler 
 
 
-class islemKaydi:
+class IslemKaydi:
     def __init__(self,aciklama,miktar):
         self.aciklama = aciklama
         self.tutar = miktar 
